e0a0cea5a03fec96e7bca5486b583eadeca771ec
rabia743 <saniazafar193@gmail.com>
2026-05-10 07:36:23 +0500

    new features added

--- introduction-sreies in pandas.py
+++ introduction-sreies in pandas.py
@@ -3,3 +3,21 @@ data = [1, 2, 3, 4, 5]
 Series = pd.Series(data)
 print(Series)
 # its example a intergers value in pndas series
+
+# example  2
+import pandas as pd
+data = [1.22, 2.33, 3.678, 4.290, 5.987]
+Series = pd.Series(data)
+print(Series)
+
+# example 3
+import pandas as pd
+data = ["rabia","sania","faira"]
+Series = pd.Series(data)
+print(Series)
+
+# example 4
+import pandas as pd
+data = [True,False]
+Series = pd.Series(data)
+print(Series)
